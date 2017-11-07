13ed789acfc522c8a0f9cde1851a9233ef3369b2
kessaras <theodoroupaulos@gmail.com>
2017-11-07 07:36:12 +0200

    Update fingerprint to Nougat v29A

--- aoscp.mk
+++ aoscp.mk
@@ -15,5 +15,5 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="g4__tmo_us" \
-    BUILD_FINGERPRINT="lge/p1__tmo_us/p1:7.0/NBD90Z/152940055675e:user/release-keys" \
-    PRIVATE_BUILD_DESC="p1__tmo_us-user 7.0 NBD90Z 152940055675e release-keys"
+    BUILD_FINGERPRINT="lge/p1__tmo_us/p1:7.0/NRD90U/171931902b165:user/release-keys" \
+    PRIVATE_BUILD_DESC="p1__tmo_us-user 7.0 NRD90U 171931902b165 release-keys"
