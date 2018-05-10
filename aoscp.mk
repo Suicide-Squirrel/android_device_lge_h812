9d7c8f236a6a16e6a952497963ad49523f33e7b7
pavlaras <paul@cypheros.co>
2018-05-10 13:41:32 +0200

    h811: Fix vendor image mismatch error * Followed by https://gerrit.cypheros.co/#/c/CypherOS/build/+/4252/
    Change-Id: I662c44c5106ee5682b176e20028f93a670497b76

--- aoscp.mk
+++ aoscp.mk
@@ -15,5 +15,6 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="g4__tmo_us" \
-    BUILD_FINGERPRINT="lge/p1__tmo_us/p1:8.0/NRD90U/1717709471d0f:user/release-keys" \
     PRIVATE_BUILD_DESC="p1__tmo_us-user 8.0 NRD90U 1717709471d0f release-keys"
+
+BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:8.0/NRD90U/1717709471d0f:user/release-keys"
