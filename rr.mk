b51b81a081c7ecb11a5fa76a5da8f2efd2daed32
steadfasterX <steadfastX@boun.cr>
2018-05-29 12:56:42 +0200

    h812: RR bring up
    (cherry picked from commit 00e2cd97cf7dad483cb29f09d125f21ba9a3b6ba)
    
    signature: Update to a proper Oreo signature (8.0)
    
    Change-Id: Ibee655fda94d30b98fe19787c346ace41d115b12
    
    (cherry picked from commit 4a69ddc5d92bf1ab084729ccac592688bdd57ca1)

--- rr.mk
+++ rr.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/h812_usu/full_h812.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/lineage to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h812_usu
+PRODUCT_NAME := rr_h812_usu
 PRODUCT_DEVICE := h812
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H812
@@ -17,4 +17,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g4__tmo_us" \
     PRIVATE_BUILD_DESC="p1__tmo_us-user 8.0 NRD90U 1717709471d0f release-keys"
 
-BUILD_FINGERPRINT="lge/p1__tmo_us/p1:8.0/NRD90U/1717709471d0f:user/release-keys"
+BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:8.1.0/OPM4.171019.016.A1/4720811:user/release-keys"
